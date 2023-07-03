bf0fd2722eb4453e7b74174b065375ef46bb210a
Matthijs Boet <m.boet@chello.nl>
2023-07-03 15:22:31 +0200

    help

--- grafbase/grafbase.config.ts
+++ grafbase/grafbase.config.ts
@@ -20,6 +20,7 @@ const Project = g.model('Project', {
   createdBy: g.relation(() => User)
 });
 
+<<<<<<< HEAD
 const jwt = auth.JWT({
 	issuer: 'grafbase',
 	secret: g.env('NEXTAUTH_SECRET'),
@@ -27,4 +28,16 @@ const jwt = auth.JWT({
 
 export default config({
   schema: g
+=======
+export default config({
+  schema: g
+  // Integrate Auth
+  // https://grafbase.com/docs/auth
+  // auth: {
+  //   providers: [authProvider],
+  //   rules: (rules) => {
+  //     rules.private()
+  //   }
+  // }
+>>>>>>> 6175946 (added grafbase schema and env file)
 })
